b4e8e153a8979828d913a786beb89e5e2931e5c5
Henrik Lissner <henrik@lissner.net>
2018-02-20 20:10:44 -0500

    Add LICENSE & boilerplate

--- counsel-css.el
+++ counsel-css.el
@@ -1,6 +1,6 @@
 ;;; counsel-css.el --- stylesheet-selector-aware swiper
 ;;
-;; Copyright (C) 2016-17 Henrik Lissner
+;; Copyright (C) 2016-2018 Henrik Lissner
 ;;
 ;; Author: Henrik Lissner <http://github/hlissner>
 ;; Maintainer: Henrik Lissner <henrik@lissner.net>
@@ -12,6 +12,19 @@
 ;; Package-Requires: ((counsel "0.7.0") (cl-lib "0.5"))
 ;;
 ;; This file is not part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
